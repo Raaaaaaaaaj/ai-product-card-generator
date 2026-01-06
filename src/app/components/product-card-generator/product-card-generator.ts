06f20e07568f3bb0e9a066391ae2b5687811ef15
Raaaaaaaaaj <143274616+Raaaaaaaaaj@users.noreply.github.com>
2026-01-06 23:10:39 +0530

    First Commit
    Made the folder structure
    1. Made the UI
    2. Wrote the service logic in aiservice
    3. Wrote a model
    4. Fetched the response from ai model
    5. Implemented a loader until the data gets processed
    6. Rendered the product card

---
+++ src/app/components/product-card-generator/product-card-generator.ts
@@ -0,0 +1,61 @@
+import { Component } from '@angular/core';
+import { Product } from '../../models/product.model';
+import { Aiservice } from '../../services/aiservice';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+@Component({
+  selector: 'app-product-card-generator',
+  imports: [FormsModule, CommonModule],
+  templateUrl: './product-card-generator.html',
+  styleUrl: './product-card-generator.css',
+})
+export class ProductCardGenerator {
+  productName = '';
+  category = '';
+
+  isLoading = false;
+  productData: Product | null = null;
+  error = '';
+
+  constructor(private aiService: Aiservice) { }
+
+  generateProduct() {
+    if (!this.productName || !this.category) {
+      this.error = 'Please enter product name and category';
+      return;
+    }
+
+    this.error = '';
+    this.isLoading = true;
+    this.productData = null;
+
+    this.aiService
+      .generateProductDetails(this.productName, this.category)
+      .subscribe({
+        next: (res) => {
+          try {
+            const rawText =
+              res.candidates[0].content.parts[0].text;
+            console.log('AI Response:', rawText);
+            if (!rawText) {
+              throw new Error("Empty AI response")
+            }
+            const cleanedText = rawText
+              .replace(/```json/g, '')
+              .replace(/```/g, '')
+              .trim();
+            this.productData = JSON.parse(cleanedText);
+            console.log('Parsed Product Data:', this.productData);
+          } catch (e) {
+            this.error = 'Failed to parse AI response';
+          } finally {
+            this.isLoading = false;
+          }
+        },
+        error: () => {
+          this.error = 'Something went wrong. Please try again.';
+          this.isLoading = false;
+        }
+      });
+  }
+}
---
+++ src/app/services/aiservice.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+@Injectable({
+  providedIn: 'root',
+})
+export class Aiservice {
+  private apiUrl =
+    'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent';
+
+  constructor(private http: HttpClient) {}
+
+  generateProductDetails(productName: string, category: string) {
+
+    const prompt = `
+You are an assistant that generates product card content.
+
+Generate:
+1. Product title
+2. Short description (2 lines)
+3. 5 keywords
+
+Product Name: ${productName}
+Category: ${category}
+
+Return result strictly in JSON format:
+{
+  "title": "",
+  "description": "",
+  "keywords": []
+}
+    `;
+
+    const body = {
+      contents: [
+        {
+          parts: [{ text: prompt }]
+        }
+      ]
+    };
+
+    return this.http.post<any>(
+      `${this.apiUrl}?key=${environment.geminiApiKey}`,
+      body
+    );
+  }
+}
